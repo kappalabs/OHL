eb2791914cada450187e3a26d8a7506a0c3c6fa1
KappaLabs <fojjta@gmail.com>
2016-03-12 11:31:31 +0100

    [FEATURE] Minor Handy Things
     - changed accesibility of fields in Place object

--- src/com/kappa_labs/ohunter/lib/entities/Place.java
+++ src/com/kappa_labs/ohunter/lib/entities/Place.java
@@ -6,6 +6,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 /**
@@ -23,13 +24,15 @@ public class Place implements Serializable {
     public double latitude;
     /**
      * Stores information given from Google Place Details request.
+     * Always not null.
      */
-    public HashMap<String, String> gfields;
+    private final HashMap<String, String> gfields;
     
     /**
      * Stores all photos assigned to this specific place.
+     * Always not null.
      */
-    public transient ArrayList<Photo> photos;
+    private transient ArrayList<Photo> photos;
 
     
     /**
@@ -58,12 +61,90 @@ public class Place implements Serializable {
         gfields.put("place_id", placeID);
     }
     
+    /**
+     * Gets the number of photos stored in this place.
+     * 
+     * @return The number of photos stored in this place.
+     */
+    public int getNumberOfPhotos() {
+        return photos.size();
+    }
+    
+    /**
+     * Puts given field value to hash table with field name as key.
+     * 
+     * @param fieldName Value used as key to internal hash table.
+     * @param fieldValue Value of that is stored to the hash table.
+     * @return Previous value with the same key.
+     */
+    public String putGField(String fieldName, String fieldValue) {
+        return gfields.put(fieldName, fieldValue);
+    }
+    
+    /**
+     * Gets field with given name from internal hash table.
+     * 
+     * @param fieldName The name of field to retrieve.
+     * @return The retrieved field, null if does not exist.
+     */
+    public String getGField(String fieldName) {
+        return gfields.get(fieldName);
+    }
+
+    /**
+     * Gets the whole hash table of gFields.
+     * 
+     * @return The whole hash table of gFields.
+     */
+    public HashMap<String, String> getGfields() {
+        return gfields;
+    }
+    
+    /**
+     * Adds a photo to internal list.
+     * 
+     * @param photo Photo to be added into the list.
+     * @return True if the list changed as a result of the call.
+     */
+    public boolean addPhoto(Photo photo) {
+        return photos.add(photo);
+    }
+    
+    /**
+     * Adds collection of photos to the internal list.
+     * 
+     * @param addPhotos Photos to be added.
+     * @return True if this list changed as a result of the call.
+     */
+    public boolean addPhotos(Collection <? extends Photo> addPhotos) {
+        return photos.addAll(addPhotos);
+    }
+    
+    /**
+     * Returns the photo at the specified position in the list.
+     * 
+     * @param index Index of the element to return.
+     * @return The photo at the specified position in the list.
+     */
+    public Photo getPhoto(int index) {
+        return photos.get(index);
+    }
+
+    /**
+     * Gets the list of all photos for this place.
+     * 
+     * @return The list of all photos for this place.
+     */
+    public ArrayList<Photo> getPhotos() {
+        return photos;
+    }
+    
     @Override
     public String toString() {
         return "Place:"
                 + " [" + String.format("%.7f", latitude) + ";" + String.format("%.7f", longitude) + "];"
                 + " ID = " + getID() + ";"
-                + " #photos = " + ((photos == null) ? "null" : photos.size());
+                + " #photos = " + getNumberOfPhotos();
     }
     
     /**
@@ -74,18 +155,15 @@ public class Place implements Serializable {
      */
     private void writeObject(ObjectOutputStream oos) throws IOException {
         oos.defaultWriteObject();
-        ArrayList<Photo> phs = null;
-        if (photos != null) {
-            phs = new ArrayList<>();
-            for (Photo p : photos) {
-                if (p.sImage != null) {
-                    p.sImage = new SImage(p.sImage);
-                }
-                if (p._sImage != null) {
-                    p._sImage = new SImage(p._sImage);
-                }
-                phs.add(p);
+        ArrayList<Photo> phs = new ArrayList<>();
+        for (Photo p : photos) {
+            if (p.sImage != null) {
+                p.sImage = new SImage(p.sImage);
             }
+            if (p._sImage != null) {
+                p._sImage = new SImage(p._sImage);
+            }
+            phs.add(p);
         }
         oos.writeObject(phs);
     }
@@ -100,6 +178,10 @@ public class Place implements Serializable {
     private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
         ois.defaultReadObject();
         photos = (ArrayList<Photo>) ois.readObject();
+        /* Photos cannot be null */
+        if (photos == null) {
+            photos = new ArrayList<>();
+        }
     }
     
 }
